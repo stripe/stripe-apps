9e2b34ec8d486c01d19803c74d036be60f0daabe
Eric Frank <ericfrank@stripe.com>
2021-12-17 11:40:25 -0600

    Update messaging example to new SDK version

--- examples/messaging/src/components/MessageListItem.tsx
+++ examples/messaging/src/components/MessageListItem.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import {ContentListItem, Badge, Group} from '@stripe/tailor-browser-sdk/ui';
+import {
+  Badge,
+  TableRow,
+  TableCell,
+  Link,
+} from '@stripe/tailor-browser-sdk/ui';
 
 import {Message} from '../types';
-import MessageDialog from './MessageDialog';
 import {getEpochMsDisplayText} from '../utils/time';
 
 const MessageListItem = ({message}: {message: Message}) => {
-  const [open, setOpen] = React.useState(false);
-
   return (
-    <ContentListItem
-      title={
-        <Group style={{marginBottom: '4px'}}>
-          {message.subject}
-          <Badge label={getEpochMsDisplayText(message.date)} color="blue" />
-        </Group>
-      }
-      end={<MessageDialog message={message} open={open} setOpen={setOpen} />}
-      endAlign="center"
-      description={message.snippet}
-      onClick={() => setOpen(true)}
-    />
+    <TableRow>
+      <TableCell>
+        <Link href="#">{message.subject}</Link>
+      </TableCell>
+      <TableCell>
+        <Badge type="info">{getEpochMsDisplayText(message.date)}</Badge>
+      </TableCell>
+    </TableRow>
   );
 };
 
--- examples/messaging/src/utils/stripeApi.ts
+++ examples/messaging/src/utils/stripeApi.ts
@@ -1,8 +1,14 @@
 import React from 'react';
+import type {Stripe} from "stripe";
 import stripeClient from '../clients/stripe';
 
-export const useCustomer = (customerId: string) => {
-  const [customer, setCustomer] = React.useState(null);
+export const useCustomer = (customerId?: string) => {
+  if (!customerId) {
+    return null;
+  }
+
+  const [customer, setCustomer] =
+    React.useState<Stripe.Customer | Stripe.DeletedCustomer | null>(null);
 
   const loadCustomer = async () => {
     const customer = await stripeClient.customers.retrieve(customerId);
