4e6161482ab6d05ef02c8278e2ba92e278f018b5
Noah Grubb <ngrubb@stripe.com>
2022-01-10 10:21:47 -0500

    remove leftover console log

--- examples/metadata/src/views/AppSettings.tsx
+++ examples/metadata/src/views/AppSettings.tsx
@@ -17,7 +17,6 @@ const AppSettings = ({ userContext }: TailorExtensionContextValue) => {
     const getCustomers = useCallback(async () => {
         const response = await stripeClient.customers.list();
         const customers = response.data;
-        console.log('customers: ', customers)
         setCustomers(customers);
     }, [])
 
