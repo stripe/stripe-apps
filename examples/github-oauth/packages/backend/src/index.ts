616b68f9f0d637e48a654e5a70ec36f94f4ed961
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-02-07 12:00:08 -0500

    Rewrite with Jae's latest example

--- examples/github-oauth/packages/backend/src/index.ts
+++ examples/github-oauth/packages/backend/src/index.ts
@@ -1,46 +1,162 @@
 import axios, { AxiosError } from 'axios';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { createHash, randomUUID } from 'crypto';
-import express from 'express';
+import { randomUUID } from 'crypto';
+import * as date from 'date-fns';
+import express, { Handler } from 'express';
 import { readFileSync } from 'fs';
 import { createServer } from 'https';
-import { join } from 'path';
 import 'source-map-support/register';
+import { Stream } from 'stream';
 
-import { authorizationURI, client_id, client_secret, tokenURI } from './config';
+import {
+  client_id,
+  client_secret,
+  githubAPIURI,
+  githubAuthURI,
+} from './config';
 
-const codeCache = new Map();
-const verifierCache = new Map();
+// These will be replaced with an actual persistent state store like Redis or a RDBMS.
+const tokenStore = new Map<string, TokenSet>();
+const authStore = new Map<string, AuthSession>();
+
+type TokenSet = {
+  access_token: string;
+  refresh_token: string;
+  id_token: string;
+  token_type: string;
+  expires: Date;
+};
+
+type AuthSession = {
+  sessionId: string;
+};
+
+const LOGIN_URI = `${githubAuthURI}/authorize`;
+const TOKEN_URI = `${githubAuthURI}/access_token`;
+const REFRESH_URI = `${githubAuthURI}/access_token`;
+const LOGOUT_URI = 'https://dashboard.stripe.com';
+const USER_URI = `${githubAPIURI}/user`;
 
 const app = express();
 
+app.use(cookieParser(process.env.COOKIE_SIGNING_SECRET));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/auth/login', (req, res) => {
-  const { state } = req.query;
-  if (typeof state !== 'string') {
-    res.status(400).send();
-  } else {
-    const codeVerifier = randomUUID();
-    verifierCache.set(state, codeVerifier);
-    const verifierHash = createHash('sha256');
-    verifierHash.update(codeVerifier);
-    const queryParams = new URLSearchParams({
-      response_type: 'code',
-      client_id,
-      redirect_uri: 'https://localhost:8080/auth/authorize',
-      scope: 'user repo',
-      state,
-      code_challenge: verifierHash.digest('base64url'),
-      code_challenge_method: 'S256',
-    });
-    res.redirect(303, `${authorizationURI}?${queryParams.toString()}`);
+/**
+ * Here we extract the session key used. Because we cannot use cookies to set a secure session id, the
+ * only thing we can rely on is stripe's user and account IDs. These will be possible to securely verify
+ * using a signed header once this is implemented in the Stripe apps SDK (see https://stripe.com/docs/stripe-apps/authenticate)
+ */
+const verifyCaller: Handler = (req, res, next) => {
+  const sig = req.headers['stripe-signature'];
+  const userId = (req.query['user'] as string) || req.headers['stripe-user-id'];
+  const accountId =
+    (req.query['account'] as string) || req.headers['stripe-account-id'];
+  try {
+    verifyUser(userId, accountId, sig);
+    res.locals.sessionId = `${userId}----${accountId}`;
+    next();
+  } catch (e) {
+    res.sendStatus(400);
   }
+};
+
+const verifyUser = (
+  userId: string | string[] | undefined,
+  accountId: string | string[] | undefined,
+  sig: string | string[] | undefined,
+) => {
+  // TODO: Add Stripe signature verification here when it is ready
+  if (!(userId && accountId)) throw new Error('Missing user identifiers');
+};
+
+/**
+ * Here we use the authorization code provided in the auth callback to fetch and store access, id, and refresh tokens.
+ * These will allow us to call the API on behalf of the user.
+ */
+const fetchToken = async (
+  sessionId: string,
+  code: string,
+): Promise<TokenSet> => {
+  const queryParams = new URLSearchParams({
+    client_id,
+    client_secret,
+    redirect_uri: 'https://localhost:8080/auth/callback/logged-in',
+    code,
+  });
+  const response = await axios.post(TOKEN_URI, queryParams, {
+    headers: {
+      Accept: 'application/json',
+    },
+    responseType: 'json',
+  });
+  const { expires_in, ...tokens } = response.data;
+  const token = {
+    ...tokens,
+    expires: date.addSeconds(Date.now(), expires_in),
+  };
+  tokenStore.set(sessionId, token);
+  return token;
+};
+
+/**
+ * Here we fetch a new access token using a stored refresh token. Access tokens often cannot be revoked, so they have]
+ * a short expiry. However, to avoid the user having to log in again we can re-authorize using a refresh token, which
+ * if it has not been revoked will give us a new access token.
+ */
+const refreshToken = async (
+  sessionId: string,
+  refreshToken: string,
+): Promise<TokenSet> => {
+  const queryParams = new URLSearchParams({
+    grant_type: 'refresh_token',
+    client_id,
+    client_secret,
+    refresh_token: refreshToken,
+  });
+  const response = await axios.post(REFRESH_URI, queryParams, {
+    headers: {
+      Accept: 'application/json',
+    },
+    responseType: 'json',
+  });
+  const { expires_in, ...tokens } = response.data;
+  const token: TokenSet = {
+    refresh_token: refreshToken, // if the response does not include a new refresh token, we keep the one we have
+    ...tokens,
+    expires: date.addSeconds(Date.now(), expires_in),
+  };
+  tokenStore.set(sessionId, token);
+  return token;
+};
+
+/**
+ * This is the URL that will be opened in a separate tab by the app. It will redirect to the OAuth tenant's login page
+ */
+app.get('/auth/login', verifyCaller, (req, res) => {
+  const state = randomUUID();
+  authStore.set(state, {
+    sessionId: res.locals.sessionId,
+  });
+  const queryParams = new URLSearchParams({
+    response_type: 'code',
+    client_id,
+    redirect_uri: 'https://localhost:8080/auth/callback/logged-in',
+    state,
+    scope: 'user repo',
+  });
+  res.redirect(303, `${LOGIN_URI}?${queryParams.toString()}`);
 });
 
-app.get('/auth/authorize', (req, res) => {
+/**
+ * Once the user has given the OAuth server their consent, they will be redirected to this path. The URL
+ * will contain a code that can be exchanged for tokens and a state parameter that allows us to associate
+ * them with the user's original request.
+ */
+app.get('/auth/callback/logged-in', (req, res) => {
   const { state, code } = req.query;
   if (
     typeof state !== 'string' ||
@@ -50,103 +166,71 @@ app.get('/auth/authorize', (req, res) => {
   ) {
     res.status(400).send();
   } else {
-    if (verifierCache.has(state)) {
-      codeCache.set(state, code);
-      res.send('Authenticated! Close this tab or window to return to Stripe');
+    const storedSession = authStore.get(state);
+    if (!storedSession) {
+      res.send(401);
+      res.send(`Didn't work. State: ${state}, Code: ${code}`);
     } else {
-      res
-        .status(401)
-        .send(
-          'State parameter not recognized. Please try authenticating again.',
-        );
-    }
-  }
-});
-
-app.get('/auth/token', (req, res) => {
-  const { state } = req.query;
-  if (typeof state !== 'string' || !state) {
-    res.status(400).send();
-  } else {
-    const verifier = verifierCache.get(state);
-    const code = codeCache.get(state);
-    if (code && verifier) {
-      const queryParams = new URLSearchParams({
-        grant_type: 'authorization_code',
-        client_id,
-        client_secret,
-        redirect_uri: 'https://localhost:8080/auth/authorize',
-        code,
-        code_verifier: verifier,
-      });
-      axios
-        .post<JSON>(tokenURI, queryParams, {
-          headers: {
-            Accept: 'application/json',
-          },
+      authStore.delete(state);
+      fetchToken(storedSession.sessionId, code)
+        .then(() => {
+          res.send(
+            'Successfully authenticated. Please close this tab to return to Stripe.',
+          );
         })
-        .then(codeResponse => {
-          res.set('content-type', codeResponse.headers['content-type']);
-          res.status(codeResponse.status);
-          res.send(codeResponse.data);
-        })
-        .catch((error: AxiosError<JSON>) => {
+        .catch((error: AxiosError) => {
           if (error.response) {
-            const codeResponse = error.response;
-            res.set('content-type', codeResponse.headers['content-type']);
-            res.status(codeResponse.status);
-            res.send(codeResponse.data);
+            console.error(error);
+            res.sendStatus(401);
           } else {
             res.status(503).send('Cannot contact authentication server');
           }
         });
-    } else {
-      res
-        .status(401)
-        .send(
-          'State parameter not recognized. Please try authenticating again.',
-        );
     }
   }
 });
 
-app.get('/auth/refresh-token', (req, res) => {
-  const { token } = req.query;
-  if (typeof token !== 'string' || !token) {
-    res.status(400).send();
-  } else {
-    const queryParams = new URLSearchParams({
-      grant_type: 'refresh_token',
-      refresh_token: token,
-      client_id,
-      client_secret,
-      redirect_uri: 'https://localhost:8080/auth/authorize',
-    });
-    axios
-      .post<JSON>(tokenURI, queryParams, {
+app.get('/auth/logout', verifyCaller, (req, res) => {
+  tokenStore.delete(res.locals.sessionId);
+  res.redirect(303, LOGOUT_URI);
+});
+
+/**
+ * This is an example of calling a protected API. We fetch the access token from our cache, renewing it with the
+ * refresh token if it is expired, and use it to make a request on behalf of the user.
+ *
+ * Another approach to calling protected APIs would be to have an API that the frontend could use to fetch the access
+ * token, which it would then use to contact the APIs directly. This is less secure but should still be acceptable since
+ * access tokens are only valid for a short period of time and the server would still control the refresh mechanism.
+ */
+app.get('/auth/repositories', verifyCaller, async (req, res) => {
+  try {
+    let token = tokenStore.get(res.locals.sessionId);
+    if (token) {
+      if (token.expires < new Date()) {
+        token = await refreshToken(res.locals.sessionId, token.refresh_token);
+      }
+      const response = await axios.get<Stream>(USER_URI, {
+        responseType: 'stream',
         headers: {
-          Accept: 'application/json',
+          authorization: `${token.token_type} ${token.access_token}`,
         },
-      })
-      .then(codeResponse => {
-        res.set('content-type', codeResponse.headers['content-type']);
-        res.send(codeResponse.data);
-      })
-      .catch((error: AxiosError<JSON>) => {
-        if (error.response) {
-          const codeResponse = error.response;
-          res.set('content-type', codeResponse.headers['content-type']);
-          res.status(codeResponse.status);
-          res.send(codeResponse.data);
-        } else {
-          res.status(503).send('Cannot contact authentication server');
-        }
       });
+      res.status(response.status);
+      response.data.pipe(res);
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(503);
   }
 });
 
-const options = {
-  key: readFileSync(join(__dirname, '../server.key')),
-  cert: readFileSync(join(__dirname, '../server.cert')),
-};
-createServer(options, app).listen(8080, () => console.log('Server started'));
+createServer(
+  {
+    key: readFileSync('server.key'),
+    cert: readFileSync('server.cert'),
+  },
+  app,
+).listen(8080, () => console.log('Server started'));
--- examples/github-oauth/packages/frontend/src/views/App.tsx
+++ examples/github-oauth/packages/frontend/src/views/App.tsx
@@ -1,12 +1,254 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Reducer, useCallback, useEffect, useReducer } from 'react';
+
 import type { TailorExtensionContextValue } from '@stripe/ui-extension-sdk/context';
-import { ContextView } from '@stripe/ui-extension-sdk/ui';
+import { Box, Button, ContextView } from '@stripe/ui-extension-sdk/ui';
 
-import { Auth } from '../components/Auth';
+const LOGIN_URI = 'https://localhost:8080/auth/login';
+const LOGOUT_URI = 'https://localhost:8080/auth/logout';
+const INFO_URI = 'https://localhost:8080/auth/repositories';
 
-const App = ({ userContext, environment }: TailorExtensionContextValue) => {
+declare global {
+  interface Crypto {
+    randomUUID: () => string;
+  }
+}
+
+type UserInfo = {
+  login: string;
+  name: string;
+  email: string;
+};
+
+type State =
+  | { name: 'initializing' }
+  | {
+      name: 'logged-out';
+    }
+  | { name: 'logging-out' }
+  | { name: 'waiting-for-auth' }
+  | { name: 'logged-in'; context: { user: UserInfo } };
+
+type Action =
+  | { type: 'initialized'; payload: { user: UserInfo | null } }
+  | { type: 'log-in' }
+  | { type: 'authorized'; payload: { user: UserInfo } }
+  | { type: 'log-out' }
+  | { type: 'session-deleted' };
+
+const initialState = (): State => ({
+  name: 'initializing',
+});
+
+// The following is a simple state machine implemented in plain React. However, in your app you might
+// prefer using a library such as XState, Redux or Little State Machine for the same purpose.
+const reducer: Reducer<State, Action> = (prevState, action) => {
+  const fallthrough = () => {
+    console.error('Invalid action', action.type, 'for state', prevState.name);
+    return prevState;
+  };
+  switch (prevState.name) {
+    case 'initializing': {
+      switch (action.type) {
+        case 'initialized':
+          return action.payload.user
+            ? { name: 'logged-in', context: { user: action.payload.user } }
+            : {
+                name: 'logged-out',
+                context: { stateKey: window.crypto.randomUUID() },
+              };
+        default:
+          return fallthrough();
+      }
+    }
+    case 'logged-out': {
+      switch (action.type) {
+        case 'log-in':
+          return {
+            name: 'waiting-for-auth',
+          };
+        default:
+          return fallthrough();
+      }
+    }
+    case 'waiting-for-auth': {
+      switch (action.type) {
+        case 'authorized':
+          return {
+            name: 'logged-in',
+            context: { user: action.payload.user },
+          };
+        case 'log-out':
+          return { name: 'logging-out' };
+        default:
+          return fallthrough();
+      }
+    }
+    case 'logging-out': {
+      switch (action.type) {
+        case 'session-deleted':
+          return initialState();
+        default:
+          return fallthrough();
+      }
+    }
+    case 'logged-in': {
+      switch (action.type) {
+        case 'log-out':
+          return { name: 'logging-out' };
+        default:
+          return fallthrough();
+      }
+    }
+    default:
+      return fallthrough();
+  }
+};
+
+const App = ({ userContext }: TailorExtensionContextValue) => {
+  const [state, dispatch] = useReducer(reducer, null, initialState);
+  const fetchWithCredentials = useCallback(
+    (uri: string, { headers, ...options }: RequestInit = {}) => {
+      const headersObject = new Headers(headers);
+      headersObject.append('stripe-user-id', userContext?.id ?? '');
+      headersObject.append('stripe-account-id', userContext?.account.id ?? '');
+      return fetch(uri, {
+        ...options,
+        headers: headersObject,
+      });
+    },
+    [],
+  );
+  useEffect(() => {
+    switch (state.name) {
+      case 'initializing': {
+        // The user may or may not be logged in, so we try fetching their information
+        // If the request fails because they are not logged in, only then will we show
+        // them the log in button
+        const controller = new AbortController();
+        fetchWithCredentials(INFO_URI, {
+          signal: controller.signal,
+        })
+          .then(res =>
+            res.json().then(user =>
+              dispatch({
+                type: 'initialized',
+                payload: { user },
+              }),
+            ),
+          )
+          .catch(() =>
+            dispatch({
+              type: 'initialized',
+              payload: { user: null },
+            }),
+          );
+        return () => {
+          controller.abort();
+        };
+      }
+      case 'waiting-for-auth': {
+        // While the user logs in and consents to our app's scopes in another tab or window
+        // we are continually polling the API. Once the user is logged in, the API will return
+        // successfully and we know the user is logged in.
+        const interval = setInterval(() => {
+          fetchWithCredentials(INFO_URI).then(res =>
+            res.json().then(
+              user =>
+                user &&
+                dispatch({
+                  type: 'authorized',
+                  payload: { user },
+                }),
+            ),
+          );
+        }, 5000);
+        return () => clearInterval(interval);
+      }
+      case 'logging-out': {
+        // Since logging out also occurs in a separate window, we could use the same polling technique
+        // as with logging in, but because logging out is quick and is very unlikely to fail, it's simpler
+        // to assume it will have worked after 1 second and return the frontend to the "initializing" state
+        // to check
+        const timeout = setTimeout(
+          () => dispatch({ type: 'session-deleted' }),
+          1000,
+        );
+        return () => clearTimeout(timeout);
+      }
+    }
+  }, [state, dispatch]);
+
+  const actions = (state: State) => {
+    switch (state.name) {
+      case 'initializing':
+        return 'Loading...';
+      case 'logged-out':
+        return (
+          <Button
+            type="primary"
+            // We must both open the login screen in a separate tab or window and kick off the polling process
+            // A link with target _blank and an onPress handler allows us to accomplish this double purpose
+            href={`${LOGIN_URI}?${new URLSearchParams({
+              account: userContext!.account.id,
+              user: userContext!.id,
+            })}`}
+            target="_blank"
+            onPress={() => dispatch({ type: 'log-in' })}
+          >
+            Log In
+          </Button>
+        );
+      case 'waiting-for-auth':
+        return (
+          <>
+            <Box>Please complete authorization in popup.</Box>
+            <Button
+              type="destructive"
+              onPress={() => dispatch({ type: 'log-out' })}
+            >
+              Cancel
+            </Button>
+          </>
+        );
+      case 'logged-in':
+        return (
+          <>
+            <Box css={{ paddingBottom: 'medium' }}>
+              You are logged in as GitHub user {state.context.user.login}.
+            </Box>
+            <Box css={{ paddingBottom: 'medium' }}>
+              <Button
+                href={`https://github.com/${state.context.user.login}`}
+                target="_blank"
+              >
+                Visit your profile.
+              </Button>
+            </Box>
+            <Button
+              type="destructive"
+              href={`${LOGOUT_URI}?${new URLSearchParams({
+                account: userContext!.account.id,
+                user: userContext!.id,
+              })}`}
+              target="_blank"
+              onPress={() => dispatch({ type: 'log-out' })}
+            >
+              Log Out
+            </Button>
+          </>
+        );
+      case 'logging-out':
+        return (
+          <Button type="destructive" disabled>
+            Log Out
+          </Button>
+        );
+    }
+  };
   return (
-    <ContextView title="Log in with GitHub using OAuth">
-      <Auth />
+    <ContextView title="Get started with Stripe Apps">
+      <Box slot="actions">{actions(state)}</Box>
     </ContextView>
   );
 };
