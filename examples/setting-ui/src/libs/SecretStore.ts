41674889b627506be5a999ff01d7a118e62c5922
Hidetaka Okamoto <hideokamoto@stripe.com>
2022-03-29 11:45:41 +0900

    examples[setting-ui]: Update the secret store API

--- examples/setting-ui/src/libs/SecretStore.ts
+++ examples/setting-ui/src/libs/SecretStore.ts
@@ -7,27 +7,37 @@ export type Options = {
 }
 
 export class SecretStore {
-    private readonly _dbName: string;
     private readonly _client: Stripe = stripeClient;
-    public constructor(dbName = "dummyproduct") {
-        this._dbName = dbName;
+
+    private _getAPIPath(method: 'GET' | 'POST', userId: string, secretName: string) {
+        const queryStrings = [
+            'scope[type]=user',
+            `scope[user]=${userId}`,
+            `name=${secretName}`,
+        ];
+        if (method === 'GET') queryStrings.push('expand[]=payload');
+        const queryString = queryStrings.join('&');
+        if (method === 'GET') {
+            return `apps/secrets/find?${queryString}`;
+        }
+        return `apps/secrets?${queryString}`;
     }
 
-    private _getAPIPath(userId: string, secretName: string) {
-        return `apps/secrets?scope[type]=user&scope[id]=${userId}&name=${secretName}&expand[]=payload`
+    private _createSecretResource(method: 'GET' | 'POST', userId: string, secretName: string,) {
+        const resource = Stripe.StripeResource.extend({
+            request: Stripe.StripeResource.method({
+                method,
+                path: this._getAPIPath(method, userId, secretName),
+            }),
+        });
+        return resource;
     }
 
     /**
      * Call the secret store API
      */
     private async _getSecretResource(userId: string, secretName: string): Promise<string | null> {
-        const getSecretResource = Stripe.StripeResource.extend({
-            request: Stripe.StripeResource.method({
-                method: 'GET',
-                path: this._getAPIPath(userId, secretName),
-            }),
-        });
-        
+        const getSecretResource = this._createSecretResource('GET', userId, secretName);
         return new Promise((resolve, reject) => {
             new getSecretResource(this._client).request({}, function(err: Error, secret: {
                 payload: string | null;
@@ -39,12 +49,7 @@ export class SecretStore {
     }
 
     private async _saveSecretResource(userId: string, secretName: string, secretValue?: string | null): Promise<void> {
-        const createSecretResource = Stripe.StripeResource.extend({
-            request: Stripe.StripeResource.method({
-                method: 'POST',
-                path: this._getAPIPath(userId, secretName),
-            })
-        });
+        const createSecretResource = this._createSecretResource('POST', userId, secretName);
         
         // Add secret to the Secret Store API; returns either error or secret object.
         return new Promise((resolve, reject) => {
