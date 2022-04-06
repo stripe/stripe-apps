4693e858e938414b9f11efe04531dd03bf4c372f
ericfrank-stripe <93341286+ericfrank-stripe@users.noreply.github.com>
2022-04-06 18:11:27 -0500

    Update SDK to 6.0.0 (#387)

--- examples/messaging/src/utils/stripeApi.ts
+++ examples/messaging/src/utils/stripeApi.ts
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import type {Stripe} from "stripe";
 import stripeClient from '../clients/stripe';
+import {getDashboardUserEmail} from '@stripe/ui-extension-sdk/utils';
 
 export const useCustomer = (customerId?: string) => {
   if (!customerId) {
@@ -18,6 +19,22 @@ export const useCustomer = (customerId?: string) => {
   useEffect(() => {
     loadCustomer();
   }, [customerId]);
+
+  return customer;
+};
+
+export const useDashboardUserEmail = () => {
+  const [customer, setCustomer] = useState<string | null>(null);
+
+  const loadEmail = async () => {
+    const resp = await getDashboardUserEmail();
+
+    setCustomer((resp?.json as any)?.email);
+  };
+
+  useEffect(() => {
+    loadEmail();
+  }, []);
 
   return customer;
 };
