f5834dec9056489e4bdbbfed0cb74fd9d210592b
vyktoremario <umehvictormario@gmail.com>
2022-04-07 16:40:45 +0100

    Added ui-testing example

---
+++ examples/ui-testing/README.md
@@ -0,0 +1,6 @@
+# UI Testing Example
+This is an example code that demonstrates testing a single view with very basic form.
+
+## Features Demonstrated
+- Snapshot tests
+- Remote-UI testing limitations
---
+++ examples/ui-testing/src/views/App.tsx
@@ -0,0 +1,56 @@
+import {
+  Box,
+  Button,
+  ContextView,
+  FormFieldGroup,
+  Notice,
+  TextField,
+} from '@stripe/ui-extension-sdk/ui';
+import type { ExtensionContextValue } from '@stripe/ui-extension-sdk/context';
+import { useState } from 'react';
+
+const App = ({ userContext, environment }: ExtensionContextValue) => {
+  const [fullName, setFullName] = useState<string>();
+  const [show, setShow] = useState<boolean>(false);
+  const [type, setType] = useState<'success' | 'error'>();
+
+  const handleSubmit = () => {
+    fullName ? setType('success') : setType('error');
+    setShow(true);
+  };
+  return (
+    <ContextView title="Create your first Stripe app">
+      <Box
+        css={{
+          padding: 'large',
+          backgroundColor: 'container',
+          fontFamily: 'monospace',
+          borderRadius: 'small',
+        }}
+      >
+        <FormFieldGroup legend="Enter Full Name" layout="column">
+          <TextField
+            placeholder="Full Name"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+          <Button onPress={handleSubmit}>Submit</Button>
+        </FormFieldGroup>
+        {show && (
+          <Notice
+            type={type == 'success' ? 'positive' : 'negative'}
+            title={type}
+            description={
+              type == 'success'
+                ? 'Added full Name!'
+                : 'First name or last name not entered!'
+            }
+            onDismiss={() => setShow(false)}
+          />
+        )}
+      </Box>
+    </ContextView>
+  );
+};
+
+export default App;
