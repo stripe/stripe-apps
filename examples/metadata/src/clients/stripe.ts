0962fb6975ba1cd47cd8f51a613b05f82544511e
Jonathan Cipriano <jcipriano@stripe.com>
2022-05-18 12:27:23 -0700

    Removing old code examples

--- examples/metadata/src/clients/stripe.ts
+++
@@ -1,9 +0,0 @@
-import Stripe from 'stripe';
-import {createHttpClient, STRIPE_API_KEY} from '@stripe/ui-extension-sdk/http_client';
-
-const stripeClient = new Stripe(STRIPE_API_KEY, {
-  httpClient: createHttpClient(),
-  apiVersion: '2020-08-27',
-});
-
-export default stripeClient;
