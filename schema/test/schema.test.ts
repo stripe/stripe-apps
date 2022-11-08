b7d28ac76c86551169018d70d26e098d61a80dcc
Jack Okerman <jackokerman@stripe.com>
2022-11-07 17:34:58 -0800

    Add local manifest schema with extends field

--- schema/test/schema.test.ts
+++ schema/test/schema.test.ts
@@ -1,14 +1,14 @@
-import Ajv from "ajv";
+import Ajv2019 from "ajv/dist/2019";
 import addFormats from "ajv-formats";
 import schema from "../stripe-app.schema.json";
+import baseSchema from "../stripe-app-base.schema.json";
+import localSchema from "../stripe-app-local.schema.json";
 
-const ajv = new Ajv();
+const ajv = new Ajv2019({ schemas: [baseSchema] });
 addFormats(ajv);
 
 // markdownDescription is not part of the JSON Schema standard, it's specific to VSCode
 ajv.addKeyword("markdownDescription");
-
-const validate = ajv.compile(schema);
 
 const basicManifest = Object.freeze({
   id: "com.example.app",
@@ -52,99 +52,150 @@ const basicManifest = Object.freeze({
 });
 
 describe("Validate manifests", () => {
-  it("validates a basic manifest", () => {
-    const valid = validate(basicManifest);
-    expect(valid).toBe(true);
-  });
+  describe("Main manifest schema", () => {
+    const validate = ajv.compile(schema);
 
-  it("rejects an empty manifest", () => {
-    const valid = validate({});
-    expect(valid).toBe(false);
-  });
-
-  it("accepts underscores in app id", () => {
-    const valid = validate({
-      ...basicManifest,
-      id: "app_id.stripe.com",
+    it("validates a basic manifest", () => {
+      expect(validate).toBeDefined();
+      const valid = validate(basicManifest);
+      expect(valid).toBe(true);
     });
-    expect(valid).toBe(true);
-  });
 
-  it("rejects invalid permissions", () => {
-    const valid = validate({
-      ...basicManifest,
-      permissions: [
-        {
-          permission: "fake_permission",
-          purpose: "Cool purpose",
-        },
-      ],
+    it("rejects an empty manifest", () => {
+      const valid = validate({});
+      expect(valid).toBe(false);
     });
-    expect(valid).toBe(false);
-  });
 
-  it("rejects invalid viewports", () => {
-    const valid = validate({
-      ...basicManifest,
-      ui_extension: {
-        ...basicManifest.ui_extension,
-        views: [
+    it("accepts underscores in app id", () => {
+      const valid = validate({
+        ...basicManifest,
+        id: "app_id.stripe.com",
+      });
+      expect(valid).toBe(true);
+    });
+
+    it("rejects invalid permissions", () => {
+      const valid = validate({
+        ...basicManifest,
+        permissions: [
           {
-            viewport: "fake.viewport",
-            component: "View",
+            permission: "fake_permission",
+            purpose: "Cool purpose",
           },
         ],
-      },
+      });
+      expect(valid).toBe(false);
     });
-    expect(valid).toBe(false);
-  });
 
-  it("rejects CSP connect-src without a path", () => {
-    const valid = validate({
-      ...basicManifest,
-      ui_extension: {
-        ...basicManifest.ui_extension,
-        content_security_policy: {
-          "connect-src": ["https://no-path.com/"],
-          purpose: "Test",
+    it("rejects invalid viewports", () => {
+      const valid = validate({
+        ...basicManifest,
+        ui_extension: {
+          ...basicManifest.ui_extension,
+          views: [
+            {
+              viewport: "fake.viewport",
+              component: "View",
+            },
+          ],
         },
-      },
+      });
+      expect(valid).toBe(false);
     });
-    expect(valid).toBe(false);
-  });
 
-  it("rejects CSP connect-src without a purpose", () => {
-    const valid = validate({
-      ...basicManifest,
-      ui_extension: {
-        ...basicManifest.ui_extension,
-        content_security_policy: {
-          "connect-src": ["https://no-path.com/"],
+    it("rejects CSP connect-src without a path", () => {
+      const valid = validate({
+        ...basicManifest,
+        ui_extension: {
+          ...basicManifest.ui_extension,
+          content_security_policy: {
+            "connect-src": ["https://no-path.com/"],
+            purpose: "Test",
+          },
         },
-      },
+      });
+      expect(valid).toBe(false);
     });
-    expect(valid).toBe(false);
+
+    it("rejects CSP connect-src without a purpose", () => {
+      const valid = validate({
+        ...basicManifest,
+        ui_extension: {
+          ...basicManifest.ui_extension,
+          content_security_policy: {
+            "connect-src": ["https://no-path.com/"],
+          },
+        },
+      });
+      expect(valid).toBe(false);
+    });
+
+    it("accepts post-install action url property for external", () => {
+      const valid = validate({
+        ...basicManifest,
+        post_install_action: {
+          type: "external",
+          url: "https://example.com",
+        },
+      });
+      expect(valid).toBe(true);
+    });
+
+    it("rejects post-install action url property for settings", () => {
+      const valid = validate({
+        ...basicManifest,
+        post_install_action: {
+          type: "settings",
+          url: "https://example.com",
+        },
+      });
+      expect(valid).toBe(false);
+    });
+
+    it('rejects additional properties, e.g. "extends"', () => {
+      const valid = validate({
+        ...basicManifest,
+        extends: "stripe-app.dev.json",
+      });
+      expect(valid).toBe(false);
+    });
   });
 
-  it("accepts post-install action url property for external", () => {
-    const valid = validate({
-      ...basicManifest,
-      post_install_action: {
-        type: "external",
-        url: "https://example.com",
-      },
-    });
-    expect(valid).toBe(true);
-  });
+  describe("Local manifest schema", () => {
+    const validate = ajv.compile(localSchema);
 
-  it("rejects post-install action url property for settings", () => {
-    const valid = validate({
-      ...basicManifest,
-      post_install_action: {
-        type: "settings",
-        url: "https://example.com",
-      },
+    it('rejects an empty manifest without an "extends" field', () => {
+      const valid = validate({});
+      expect(valid).toBe(false);
     });
-    expect(valid).toBe(false);
+
+    it.each([
+      "stripe-app.json",
+      "stripe-app.dev.json",
+      "stripe-app.prod.json",
+      "./stripe-app.json",
+      "../stripe-app.json",
+      "../schemas/stripe-app.json",
+      "../../stripe-app.json",
+      "../../../../foo/bar/stripe-app.json",
+      "../../stripe-app.json",
+    ])('accepts "extends" value: %s', (value) => {
+      const valid = validate({
+        extends: value,
+      });
+      expect(valid).toBe(true);
+    });
+
+    it("accepts an overridden CSP connect-src", () => {
+      const valid = validate({
+        extends: "stripe-app.json",
+        ui_extension: {
+          content_security_policy: {
+            "connect-src": ["https://fake-data.acme.com/api/"],
+          },
+        },
+      });
+      expect(validate.errors).toBe(null);
+    });
   });
 });
