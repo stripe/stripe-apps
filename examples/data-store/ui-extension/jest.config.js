477d63cb525cc50aafd6ab132b3528d5e0191a6a
Timothy Bennett <timbennett@stripe.com>
2024-06-13 10:23:19 -0500

    Remove references to datastore API

--- examples/data-store/ui-extension/jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-/* eslint-disable @typescript-eslint/no-var-requires */
-const UIExtensionsConfig = require("@stripe/ui-extension-tools/jest.config.ui-extension");
-
-module.exports = {
-  ...UIExtensionsConfig,
-};
--- examples/data-store/ui-extension/ui-extensions.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference types="@stripe/ui-extension-tools" />
